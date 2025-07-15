907daa2e1fc4e0f1a9d9d975e20efc13536d231a
rlaban12 <114490382+rlaban12@users.noreply.github.com>
2025-07-15 15:49:24 +0900

    20250715_11549 commit

---
+++ src/main/java/com/spring/basic/chap3_2/pratice/Feedback.java
@@ -0,0 +1,18 @@
+package com.spring.basic.chap3_2.pratice;
+
+import lombok.*;
+
+@Getter @Setter @ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Feedback {
+
+    private Long id;
+    private Long userId;
+    private String message;
+    private int rating;
+
+
+}
